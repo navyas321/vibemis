c1bb68b688e22e44c29c72a973848bbe4dca0887
navyas321 <navyas321@gmail.com>
2026-05-28 05:02:48 -0400

    fix: use shared egl.vert for all EGLRenderer shader programs (#30)
    Three compileShader() calls in EGLRenderer::compileShaders() referenced per-program vertex shaders (egl_nv12.vert, egl_opaque.vert, egl_overlay.vert) that have never existed in the repo or resources.qrc. Fixed to use the shared egl.vert that has always been the only vertex shader shipped. Confirmed working on Legion Go S Z2 (test12).

--- app/streaming/video/ffmpeg-renderers/eglvid.cpp
+++ app/streaming/video/ffmpeg-renderers/eglvid.cpp
@@ -362,7 +362,7 @@ bool EGLRenderer::compileShaders() {
 
     // XXX: TODO: other formats
     if (m_EGLImagePixelFormat == AV_PIX_FMT_NV12 || m_EGLImagePixelFormat == AV_PIX_FMT_P010) {
-        m_ShaderProgram = compileShader("egl_nv12.vert", "egl_nv12.frag");
+        m_ShaderProgram = compileShader("egl.vert", "egl_nv12.frag");
         if (!m_ShaderProgram) {
             return false;
         }
@@ -373,7 +373,7 @@ bool EGLRenderer::compileShaders() {
         m_ShaderProgramParams[NV12_PARAM_PLANE2] = glGetUniformLocation(m_ShaderProgram, "plane2");
     }
     else if (m_EGLImagePixelFormat == AV_PIX_FMT_DRM_PRIME) {
-        m_ShaderProgram = compileShader("egl_opaque.vert", "egl_opaque.frag");
+        m_ShaderProgram = compileShader("egl.vert", "egl_opaque.frag");
         if (!m_ShaderProgram) {
             return false;
         }
@@ -388,7 +388,7 @@ bool EGLRenderer::compileShaders() {
         return false;
     }
 
-    m_OverlayShaderProgram = compileShader("egl_overlay.vert", "egl_overlay.frag");
+    m_OverlayShaderProgram = compileShader("egl.vert", "egl_overlay.frag");
     if (!m_OverlayShaderProgram) {
         return false;
     }
